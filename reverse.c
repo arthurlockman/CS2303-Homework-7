435c6eff5fcc9a33dccd7a53812b7d71a218ea30
Arthur Lockman <hello@rthr.me>
2015-03-02 23:39:24 -0500

    All done.

--- reverse.c
+++ reverse.c
@@ -4,12 +4,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <libgen.h>
-
+#include <stdlib.h>
 /** Print usage message
  */
-void usage () {
-  printf("usage: reversestring \"string with characters to reverse\"\n");
-  printf("   OR: reversewords \"string with words to reverse\"\n");
+void usage ()
+{
+    printf("usage: reversestring \"string with characters to reverse\"\n");
+    printf("   OR: reversewords \"string with words to reverse\"\n");
 }
 
 /** Reverses all the characters in a string, in place.
@@ -17,49 +18,97 @@ void usage () {
  * @param s String to reverse.
  */
 
-void reversestring(char* s) {
+void reversestring(char* s)
+{
+    size_t len = strlen(s);
+    size_t i;
+    for (i = 0; i < len / 2; i++)
+    {
+        char tmp = s[i];
+        s[i] = s[len - 1 - i];
+        s[len - 1 - i] = tmp;
+    }
 }
 
 /** Reverses all the words in a string, in place.
  * For example, "hello world" becomes "world hello"
  * @param s String with words to reverse.
  */
-void reversewords(char* s) {
+void reversewords(char* s)
+{
+    reversestring(s);
+    char *t, *u;
+    char b[200];
+    int i = 0;
+    t = s;
+    while(*t)
+    {
+        while(*t != ' ' && *t != '\0')
+        {
+            b[i] = *t;
+            t++;
+            i++;
+        }
+        b[i] = '\0';
+        i = 0;
+
+        reversestring(b);
+
+        u = b;
+
+        while(*u)
+        {
+            *s = *u;
+            u++;
+            s++;
+        }
+        
+        while(*s == ' ')
+            s++;
+        t = s;
+    }
 }
 
 /** Must take exactly one argument.
  */
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
 
-  char* command; // duplicate of command name
-  char* basec;    // basename of command
-  char* s;       // String to reverse;
+    char* command; // duplicate of command name
+    char* basec;    // basename of command
+    char* s;       // String to reverse;
 
-  if (argc != 2) { // Check that it was called with exactly one argument
-    usage();
-    return 1;
-  }
+    if (argc != 2)   // Check that it was called with exactly one argument
+    {
+        usage();
+        return 1;
+    }
 
-  s = strdup(argv[1]); // Sring to reverse
+    s = strdup(argv[1]); // Sring to reverse
 
-  // See how the command was invoked
+    // See how the command was invoked
 
-  // First strip down to just the basename, no directory prefix
-  command = strdup(argv[0]);
-  basec = basename(command);
+    // First strip down to just the basename, no directory prefix
+    command = strdup(argv[0]);
+    basec = basename(command);
 
-  if (strcmp(basec, "reversestring") == 0) {
-    printf("Original string: %s\n", s);
-    reversestring(argv[1]);
-    printf("Reversed string: %s\n", s);
-    return 0;
-  } else if (strcmp(basec, "reversewords") == 0) {
-    printf("Original string: %s\n", s);
-    reversewords(argv[1]);
-    printf("Reversed string: %s\n", s);
-    return 0;
-  } else { // Those are the only two valid choices
-    usage();
-    return 1;
-  }
+    if (strcmp(basec, "reversestring") == 0)
+    {
+        printf("Original string: %s\n", argv[1]);
+        reversestring(s);
+        printf("Reversed string: %s\n", s);
+        return 0;
+    }
+    else if (strcmp(basec, "reversewords") == 0)
+    {
+        printf("Original string: %s\n", argv[1]);
+        reversewords(s);
+        printf("Reversed string: %s\n", s);
+        return 0;
+    }
+    else     // Those are the only two valid choices
+    {
+        usage();
+        return 1;
+    }
 }
